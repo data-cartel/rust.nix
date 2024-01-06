d629445080b2fef633f77db823f8030d64775688
0xgleb <gleb.dianov@protonmail.com>
2024-01-06 17:04:04 +0100

    flake init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [{
+              # https://devenv.sh/reference/options/
+              packages = with pkgs; [ cargo-watch ];
+
+              enterShell = "";
+
+              languages.rust = {
+                enable = true;
+                version = "stable";
+                packages = {
+                  rustfmt = inputs.fenix.packages.${pkgs.system}.latest.rustfmt;
+                  clippy = inputs.fenix.packages.${pkgs.system}.latest.clippy;
+                };
+              };
+            }];
+          };
+        });
+    };
+}
