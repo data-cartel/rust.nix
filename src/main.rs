05d8f088865bc2664b9498541333fb9b185b6780
0xgleb <gleb.dianov@protonmail.com>
2024-01-06 18:25:43 +0100

    parse custom values

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,37 @@ struct Env {
     verbose: bool,
     #[clap(short, long, env, default_value = "dev")]
     mode: Mode,
-    // #[clap(long, env = "ETHEREUM_ADDRESS")]
-    // eth_addr: Option<[u8; 20]>,
+    #[clap(long, env = "ETHEREUM_ADDRESS", value_parser = EthAddr::parse)]
+    eth_addr: Option<EthAddr>,
+    // #[arg(short, long, action = clap::ArgAction::Count)]
+    // verbose: u8,
+}
+
+#[derive(Clone)]
+struct EthAddr([u8; 20]);
+
+impl std::fmt::Debug for EthAddr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut hex = [0u8; 40];
+        hex::encode_to_slice(&self.0, &mut hex).unwrap();
+        write!(f, "0x{}", String::from_utf8_lossy(&hex))
+    }
+}
+
+impl EthAddr {
+    fn parse(s: &str) -> Result<Self, String> {
+        if s.len() == 42 {
+            return Self::parse(s.trim_start_matches("0x"));
+        }
+
+        match hex::decode(s) {
+            Err(err) => Err(format!("Invalid Ethereum address hex: {}", err)),
+            Ok(bytes) => bytes
+                .try_into()
+                .map(EthAddr)
+                .map_err(|_| "Invalid Ethereum address length".to_string()),
+        }
+    }
 }
 
 #[derive(Clone, Debug, ValueEnum)]
