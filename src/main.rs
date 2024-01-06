d68c7e301e3b269799d9c9f37b7ade8590a85f5b
0xgleb <gleb.dianov@protonmail.com>
2024-01-06 17:40:47 +0100

    add enum example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Debug, Parser)]
 struct Env {
@@ -6,17 +6,21 @@ struct Env {
     api_url: String,
     #[clap(long, env)]
     api_key: String,
-    #[clap(short, long, env, default_value = "false")]
+    #[clap(short, long = "debug", env = "DEBUG", default_value = "false")]
     verbose: bool,
+    #[clap(short, long, env, default_value = "dev")]
+    mode: Mode,
+    // #[clap(long, env = "ETHEREUM_ADDRESS")]
     // eth_addr: Option<[u8; 20]>,
-    // mode: Mode,
 }
 
-// #[derive(Debug)]
-// enum Mode {
-//     Dev,
-//     Prod,
-// }
+#[derive(Clone, Debug, ValueEnum)]
+enum Mode {
+    #[clap(name = "dev")]
+    Development,
+    #[clap(name = "prod")]
+    Production,
+}
 
 fn main() {
     let env = Env::parse();
