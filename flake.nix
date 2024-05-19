345b61035d8be073bb4cfce4ddd81c5eb0ac13f2
0xgleb <gleb.dianov@protonmail.com>
2024-05-19 21:10:45 +0200

    upd

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     fenix = {
@@ -10,40 +10,51 @@
   };
 
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
 
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-              packages = with pkgs; [ cargo-watch ];
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = with pkgs; [ cargo-watch ];
 
-              enterShell = "";
-              dotenv.enable = true;
+                enterShell = "";
+                dotenv.enable = true;
 
-              languages.rust = {
-                enable = true;
-                channel = "stable";
-                toolchain = {
-                  rustfmt = inputs.fenix.packages.${pkgs.system}.latest.rustfmt;
-                  clippy = inputs.fenix.packages.${pkgs.system}.latest.clippy;
+                languages.rust = {
+                  enable = true;
+                  channel = "stable";
+                  toolchain = inputs.fenix.packages.${pkgs.system}.latest;
                 };
-              };
-            }];
+              }
+            ];
           };
-        });
+        }
+      );
     };
 }
